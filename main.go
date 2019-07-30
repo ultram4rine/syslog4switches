ebc1c1dabd960cbf96e2a6f160e5f2d36a115907
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-30 16:15:57 +0400

    Init

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	syslog "gopkg.in/mcuadros/go-syslog.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var config struct {
+	DBHost string `json:"DBHost"`
+}
+
+type switchLog struct {
+	ID        bson.ObjectId `bson:"_id"`
+	SwName    string        `bson:"swname"`
+	SwIP      string        `bson:"swip"`
+	LogString string        `bson:"logstring"`
+}
+
+func main() {
+	var (
+		//confPath = "conf.json"
+		err error
+	)
+
+	/*confdata, err := ioutil.ReadFile(confPath)
+	if err != nil {
+		log.Printf("Error reading config file: %s", err)
+	}
+
+	err = json.Unmarshal(confdata, &config)
+	if err != nil {
+		log.Printf("Error unmarshalling config file: %s", err)
+	}
+
+	session, err := mgo.Dial(config.DBHost)
+	if err != nil {
+		log.Printf("Error connectiong to database: %s", err)
+	}
+	defer session.Close()
+
+	logsCollection := session.DB("logsdb").C("logs")*/
+
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+
+	server := syslog.NewServer()
+	server.SetFormat(syslog.RFC5424)
+	server.SetHandler(handler)
+
+	err = server.ListenUDP("localhost:514")
+	if err != nil {
+		log.Printf("Error configuring server for UDP listen: %s", err)
+	}
+
+	err = server.Boot()
+	if err != nil {
+		log.Printf("Error starting server: %s", err)
+	}
+
+	go func(channel syslog.LogPartsChannel) {
+		for logs := range channel {
+			fmt.Println(logs)
+		}
+	}(channel)
+
+	server.Wait()
+}
