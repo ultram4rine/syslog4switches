ab1f6c1349790749b21ecb4c0073132ef3873940
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-31 13:50:46 +0400

    Added parsing and printing log

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
+	"time"
 
 	syslog "gopkg.in/mcuadros/go-syslog.v2"
+	"gopkg.in/mcuadros/go-syslog.v2/format"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -13,10 +16,15 @@ var config struct {
 }
 
 type switchLog struct {
-	ID        bson.ObjectId `bson:"_id"`
-	SwName    string        `bson:"swname"`
-	SwIP      string        `bson:"swip"`
-	LogString string        `bson:"logstring"`
+	ID           bson.ObjectId `bson:"_id"`
+	SwName       string        `bson:"swname"`
+	SwIP         string        `bson:"swip"`
+	LogTime      string        `bson:"logtime"`
+	LogMessage   string        `bson:"logmsg"`
+	LogTimeStamp time.Time     `bson:"logtimestamp"`
+	LogFacility  int           `bson:"logfac"`
+	LogSeverity  int           `bson:"logsev"`
+	LogPriority  int           `bson:"logpri"`
 }
 
 func main() {
@@ -61,12 +69,58 @@ func main() {
 	}
 
 	go func(channel syslog.LogPartsChannel) {
-		for log := range channel {
-			for k, v := range log {
-				fmt.Println(k, ":", v)
-			}
+		for logmap := range channel {
+			l := parseLog(logmap)
+			fmt.Println("Facility:", l.LogFacility)
+			fmt.Println("Severity:", l.LogSeverity)
+			fmt.Println("Priority:", l.LogPriority)
+			fmt.Println("Switch Name:", l.SwName)
+			fmt.Println("Switch IP:", l.SwIP)
+			fmt.Println("Time:", l.LogTime)
+			fmt.Println("TimeStamp:", l.LogTimeStamp)
+			fmt.Println("Message:", l.LogMessage)
+			fmt.Println()
 		}
 	}(channel)
 
 	server.Wait()
+}
+
+func parseLog(logmap format.LogParts) switchLog {
+	var l switchLog
+
+	for key, val := range logmap {
+		switch key {
+		case "content":
+			{
+				valStr := val.(string)
+				data := strings.Split(strings.Split(valStr, ": ")[0], " ")
+
+				for i, d := range data {
+					if i < 3 {
+						l.LogTime += d + " "
+					} else {
+						switch i {
+						case 3:
+							l.SwName = d
+						}
+					}
+				}
+
+				l.LogMessage = strings.Split(valStr, ": ")[1]
+			}
+		case "client":
+			l.SwIP = strings.Split(val.(string), ":")[0]
+		case "timestamp":
+			l.LogTimeStamp = val.(time.Time)
+		case "facility":
+			l.LogFacility = val.(int)
+		case "severity":
+			l.LogSeverity = val.(int)
+		case "priority":
+			l.LogPriority = val.(int)
+		}
+	}
+
+	return l
 }
