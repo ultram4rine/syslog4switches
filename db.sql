cedb137a7d1d2197cb65196f69de4f6012980d99
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-08-01 20:37:56 +0400

    Added sql script

---
+++ db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE switchlogs
+(
+    `log_id` UInt32,
+    `ts_local` DateTime,
+    `sw_name` String,
+    `sw_ip` String,
+    `ts_remote` DateTime,
+    `facility` UInt32,
+    `severity` UInt32,
+    `priority` UInt32,
+    `log_time` String,
+    `log_msg` String
+)
+ENGINE = MergeTree()
+PARTITION BY toYYYYMMDD(ts_local)
+ORDER BY ts_local
+
