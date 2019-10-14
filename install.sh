b17feeb8a81466200e69dd2a83cf5c184acc5720
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-10-14 16:06:01 +0400

    Renamed

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp syslog-serv.service /etc/systemd/system/
+cp syslog4switches.service /etc/systemd/system/
 systemctl daemon-reload
-go build syslog-serv.go
-mv -f syslog-serv /usr/local/sbin/syslog-serv/
-cp conf.json /usr/local/sbin/syslog-serv/
-systemctl enable syslog-serv
-systemctl start syslog-serv
+go build server.go
+mv -f server /usr/local/sbin/syslog4switches/
+cp conf.json /usr/local/sbin/syslog4switches/
+systemctl enable syslog4switches
+systemctl start syslog4switches
