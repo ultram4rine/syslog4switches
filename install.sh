2e0c0f92ab5c3d75e06afb7f252c01c016b54a12
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-12-05 12:55:07 +0400

    make a directory

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 cp syslog4switches.service /etc/systemd/system/
 systemctl daemon-reload
 go build server.go
+mkdir /usr/local/sbin/syslog4switches
 mv -f server /usr/local/sbin/syslog4switches/
 cp syslog4switches.conf.json /usr/local/sbin/syslog4switches/
 systemctl enable syslog4switches
