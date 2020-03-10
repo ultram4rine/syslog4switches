f1318d14fdb49d887fbc7c54dcc1bd5386ecc2b4
ultram4rine <ultramarine730@gmail.com>
2020-03-10 16:32:20 +0400

    formatted schema

--- schema.sql
+++ schema.sql
@@ -1,18 +1,13 @@
-CREATE DATABASE IF NOT EXISTS logs
-
-USE logs
-
-CREATE TABLE IF NOT EXISTS logs.switchlogs
-(
-    `ts_local` DateTime,
-    `sw_name` String,
-    `sw_ip` IPv4,
-    `ts_remote` DateTime,
-    `facility` UInt8,
-    `severity` UInt8,
-    `priority` UInt8,
-    `log_msg` String
-)
-ENGINE = MergeTree()
-PARTITION BY toYYYYMMDD(ts_local)
-ORDER BY ts_local
+USE logs;
+CREATE TABLE IF NOT EXISTS logs.switchlogs (
+  `ts_local` DateTime,
+  `sw_name` String,
+  `sw_ip` IPv4,
+  `ts_remote` DateTime,
+  `facility` UInt8,
+  `severity` UInt8,
+  `priority` UInt8,
+  `log_msg` String
+) ENGINE = MergeTree() PARTITION BY toYYYYMMDD(ts_local)
+ORDER BY
+  ts_local
